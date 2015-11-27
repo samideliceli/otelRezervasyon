d85dab2212896aec305eaa84345a38fea995c0bb
samideliceli <samideliceli@gmail.com>
2015-11-27 22:00:00 +0200

    Proje Taslağı oluşturuldu  -Tüm sayfa sınıfları  -Tab olayı yapıldı

--- OtelRezervasyon/src/otelrezervasyon/OtelRezervasyon.java
+++ OtelRezervasyon/src/otelrezervasyon/OtelRezervasyon.java
@@ -5,6 +5,10 @@
  */
 package otelrezervasyon;
 
+import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
+import javax.swing.SwingUtilities;
+
 /**
  *
  * @author delilog
@@ -14,8 +18,39 @@ public class OtelRezervasyon {
     /**
      * @param args the command line arguments
      */
+    
+    JFrame frame = new JFrame();
+    JTabbedPane tabbedPane = new JTabbedPane();
+    
+    musteriIslemleri musteri = new musteriIslemleri();
+    odaIslemleri oda = new odaIslemleri();
+    muhasebe muhasebe=new muhasebe();
+    hizmetler hizmet= new hizmetler();
+    
+    
+     public OtelRezervasyon() { 
+        tabbedPane.add("Müşteri", musteri);
+        tabbedPane.add("Oda", oda);
+        tabbedPane.add("Hizmetler",hizmet);
+        tabbedPane.add("Muhasebe", muhasebe);
+        
+        
+        frame.getContentPane().add(tabbedPane);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setSize(640, 480);
+        // frame.pack(); 
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+    } 
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        
+         SwingUtilities.invokeLater(new Runnable() {
+            public void run() { 
+                new OtelRezervasyon(); 
+            } 
+        }); 
     }
     
 }
