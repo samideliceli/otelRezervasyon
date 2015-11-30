43a24a3fa70a6e1b886af353277f5bcba856c8f9
Berkay Kireçci <irfankirecci@stu.comu.edu.tr>
2015-12-01 00:44:27 +0200

    Veritabanı İşlemleri ve DefaultTableModel
    * Veritabanı bağlantısı eklendi.
    * Veritabanından veri çekip tabloya yazdırıldı.

--- OtelRezervasyon/src/otelrezervasyon/OtelRezervasyon.java
+++ OtelRezervasyon/src/otelrezervasyon/OtelRezervasyon.java
@@ -1,56 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package otelrezervasyon;
 
 import javax.swing.JFrame;
 import javax.swing.JTabbedPane;
 import javax.swing.SwingUtilities;
 
-/**
- *
- * @author delilog
- */
 public class OtelRezervasyon {
 
-    /**
-     * @param args the command line arguments
-     */
-    
     JFrame frame = new JFrame();
     JTabbedPane tabbedPane = new JTabbedPane();
-    
     musteriIslemleri musteri = new musteriIslemleri();
     odaIslemleri oda = new odaIslemleri();
-    muhasebe muhasebe=new muhasebe();
-    hizmetler hizmet= new hizmetler();
-    
-    
-     public OtelRezervasyon() { 
+    muhasebe muhasebe = new muhasebe();
+    hizmetler hizmet = new hizmetler();
+
+    public OtelRezervasyon() {
         tabbedPane.add("Müşteri", musteri);
         tabbedPane.add("Oda", oda);
-        tabbedPane.add("Hizmetler",hizmet);
+        tabbedPane.add("Hizmetler", hizmet);
         tabbedPane.add("Muhasebe", muhasebe);
-        
-        
+
         frame.getContentPane().add(tabbedPane);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setSize(640, 480);
         // frame.pack(); 
         frame.setLocationByPlatform(true);
         frame.setVisible(true);
-    } 
-    
-    public static void main(String[] args) {
-        // TODO code application logic here
-        
-         SwingUtilities.invokeLater(new Runnable() {
-            public void run() { 
-                new OtelRezervasyon(); 
-            } 
-        }); 
+
     }
-    
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new OtelRezervasyon();
+            }
+
+        });
+
+    }
+
 }
---
+++ OtelRezervasyon/src/otelrezervasyon/dbConnection.java
@@ -0,0 +1,26 @@
+package otelrezervasyon;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class dbConnection {
+
+    private String host = "jdbc:derby://localhost:1527/otelDb";
+    private String userName = "graylight";
+    private String userPass = "pass";
+    static Connection con;
+    static Statement st;
+
+    public void dbBaglan() {
+        try {
+            con = DriverManager.getConnection(host, userName, userPass);
+            st = (Statement) con.createStatement();
+        } catch (SQLException ex) {
+            Logger.getLogger(OtelRezervasyon.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
