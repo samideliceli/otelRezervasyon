b3aef4fe7d6ec94ffda9c1cf3fd1f3d4c67bd2a4
Berkay Kireçci <irfankirecci@stu.comu.edu.tr>
2015-12-02 00:46:52 +0200

    Hizmetler

--- OtelRezervasyon/src/otelrezervasyon/hizmetler.java
+++ OtelRezervasyon/src/otelrezervasyon/hizmetler.java
@@ -11,18 +11,24 @@ import static otelrezervasyon.dbConnection.st;
 public class hizmetler extends javax.swing.JPanel {
 
     private dbConnection db;
+    private static int sutunSayisi;
+    private static int satirSayisi;
+    private static DefaultTableModel tb;
 
     public hizmetler() {
         initComponents();
         db = new dbConnection();
         db.dbBaglan();
-
         try (ResultSet rs = st.executeQuery("SELECT * FROM SERVIS")) {
-            int sutunSayisi = rs.getMetaData().getColumnCount();
-            DefaultTableModel tb = new DefaultTableModel();
-            for (int i = 1; i <= sutunSayisi; i++) {
+            sutunSayisi = rs.getMetaData().getColumnCount();
+            satirSayisi = rs.getRow();
+            tb = new DefaultTableModel();
+            for (int i = 1;
+                    i <= sutunSayisi;
+                    i++) {
                 tb.addColumn(rs.getMetaData().getColumnName(i));
             }
+
             while (rs.next()) {
                 Object[] row = new Object[sutunSayisi];
                 for (int i = 1; i <= sutunSayisi; i++) {
@@ -32,9 +38,11 @@ public class hizmetler extends javax.swing.JPanel {
                 servisTablosu.setModel(tb);
 
             }
+
             con.close();
         } catch (SQLException hata) {
-            Logger.getLogger(OtelRezervasyon.class.getName()).log(Level.SEVERE, null, hata);
+            Logger.getLogger(OtelRezervasyon.class
+                    .getName()).log(Level.SEVERE, null, hata);
         }
 
     }
@@ -45,6 +53,7 @@ public class hizmetler extends javax.swing.JPanel {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         servisTablosu = new javax.swing.JTable();
+        guncelle = new javax.swing.JButton();
 
         servisTablosu.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -59,26 +68,45 @@ public class hizmetler extends javax.swing.JPanel {
         ));
         jScrollPane1.setViewportView(servisTablosu);
 
+        guncelle.setText("Güncelle");
+        guncelle.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                guncelleActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(13, Short.MAX_VALUE))
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addGap(157, 157, 157)
+                .addComponent(guncelle)
+                .addContainerGap(162, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(44, 44, 44)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 185, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(71, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(guncelle)
+                .addContainerGap(39, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void guncelleActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_guncelleActionPerformed
+
+
+    }//GEN-LAST:event_guncelleActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton guncelle;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable servisTablosu;
     // End of variables declaration//GEN-END:variables
