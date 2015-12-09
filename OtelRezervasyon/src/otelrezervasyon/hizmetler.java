a97799735c0c4aad5fc127d23be52ebac1f7bfcc
Berkay Kireçci <irfankirecci@stu.comu.edu.tr>
2015-12-09 16:09:18 +0200

    Güncelleme Ve Veri Ekleme
    - Güncellemedeki sorun giderildi.
    - Veri ekleyince tablo yenilenme yapılmadı.

--- OtelRezervasyon/src/otelrezervasyon/hizmetler.java
+++ OtelRezervasyon/src/otelrezervasyon/hizmetler.java
@@ -1,10 +1,12 @@
 package otelrezervasyon;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
+import javax.swing.JTable;
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
 import javax.swing.table.DefaultTableModel;
@@ -25,7 +27,6 @@ public class hizmetler extends javax.swing.JPanel {
     public hizmetler() {
         initComponents();
         db = new dbConnection();
-        db.dbBaglan();
         tb = new DefaultTableModel() {
             @Override
             public boolean isCellEditable(int row, int col) {
@@ -36,26 +37,7 @@ public class hizmetler extends javax.swing.JPanel {
             }
         };
         guncelle.setEnabled(false);
-        try (ResultSet rs = st.executeQuery("SELECT * FROM SERVIS")) {
-            sutunSayisi = rs.getMetaData().getColumnCount();
-            for (int s = 1; s <= sutunSayisi; s++) {
-                tb.addColumn(rs.getMetaData().getColumnName(s));
-            }
-
-            while (rs.next()) {
-                row = new Object[sutunSayisi];
-                for (i = 1; i <= sutunSayisi; i++) {
-                    row[i - 1] = rs.getObject(i);
-                }
-                tb.addRow(row);
-                servisTablosu.setModel(tb);
-                satirSayisi = tb.getRowCount();
-            }
-            con.close();
-        } catch (SQLException hata) {
-            Logger.getLogger(OtelRezervasyon.class
-                    .getName()).log(Level.SEVERE, null, hata);
-        }
+        tabloyuCek();
         deger = new Object[satirSayisi][sutunSayisi];
         for (satir = 0; satir < satirSayisi; satir++) {
             for (sutun = 0; sutun < sutunSayisi; sutun++) {
@@ -79,6 +61,29 @@ public class hizmetler extends javax.swing.JPanel {
         });
     }
 
+    public void tabloyuCek() {
+        db.dbBaglan();
+        try (ResultSet rs = st.executeQuery("SELECT * FROM SERVIS")) {
+            sutunSayisi = rs.getMetaData().getColumnCount();
+            for (int s = 1; s <= sutunSayisi; s++) {
+                tb.addColumn(rs.getMetaData().getColumnName(s));
+            }
+            while (rs.next()) {
+                row = new Object[sutunSayisi];
+                for (i = 1; i <= sutunSayisi; i++) {
+                    row[i - 1] = rs.getObject(i);
+                }
+                tb.addRow(row);
+                servisTablosu.setModel(tb);
+                satirSayisi = tb.getRowCount();
+            }
+            con.close();
+        } catch (SQLException hata) {
+            Logger.getLogger(OtelRezervasyon.class
+                    .getName()).log(Level.SEVERE, null, hata);
+        }
+    }
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -86,6 +91,9 @@ public class hizmetler extends javax.swing.JPanel {
         jScrollPane1 = new javax.swing.JScrollPane();
         servisTablosu = new javax.swing.JTable();
         guncelle = new javax.swing.JButton();
+        yeniServiAdi = new javax.swing.JTextField();
+        yeniServisUcret = new javax.swing.JTextField();
+        servisEkle = new javax.swing.JButton();
 
         servisTablosu.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -107,34 +115,56 @@ public class hizmetler extends javax.swing.JPanel {
             }
         });
 
+        servisEkle.setText("Ekle");
+        servisEkle.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                servisEkleActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 376, Short.MAX_VALUE)
-                .addContainerGap())
-            .addGroup(layout.createSequentialGroup()
-                .addGap(155, 155, 155)
-                .addComponent(guncelle)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 260, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(guncelle, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(servisEkle, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(yeniServisUcret)
+                    .addComponent(yeniServiAdi))
+                .addContainerGap(29, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(44, 44, 44)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 185, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(guncelle)
-                .addContainerGap(33, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(44, 44, 44)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 185, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(69, 69, 69)
+                        .addComponent(yeniServiAdi, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(yeniServisUcret, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(servisEkle)
+                        .addGap(18, 18, 18)
+                        .addComponent(guncelle)))
+                .addContainerGap(71, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
     private void guncelleActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_guncelleActionPerformed
         db.dbBaglan();
         try {
-            if (st.executeUpdate("UPDATE SERVIS SET " + sutunAdi + " = '" + sonDeger + "' WHERE " + sutunAdi + " = '" + deger[satir][sutun] + "'") != 0) {
+            String sql = ("UPDATE SERVIS SET " + sutunAdi + " = (?) WHERE " + sutunAdi + " = (?) ");
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setObject(1, sonDeger);
+            ps.setObject(2, deger[satir][sutun]);
+            if (ps.executeUpdate() != 0) {
                 JOptionPane.showMessageDialog(null, "Veri Başarıyla Güncellendi!");
                 deger[satir][sutun] = sonDeger;
             } else {
@@ -147,9 +177,30 @@ public class hizmetler extends javax.swing.JPanel {
         }
     }//GEN-LAST:event_guncelleActionPerformed
 
+    private void servisEkleActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_servisEkleActionPerformed
+        String servisAdi = yeniServiAdi.getText();
+        String servisUcreti = yeniServisUcret.getText();
+        db.dbBaglan();
+        try {
+            String sql = ("INSERT INTO SERVIS (ISIM,UCRET) VALUES (?,?)");
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, servisAdi);
+            ps.setString(2, servisUcreti);
+            ps.executeUpdate();
+            yeniServiAdi.setText("");
+            yeniServisUcret.setText("");
+            con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(hizmetler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_servisEkleActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton guncelle;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JButton servisEkle;
     private javax.swing.JTable servisTablosu;
+    private javax.swing.JTextField yeniServiAdi;
+    private javax.swing.JTextField yeniServisUcret;
     // End of variables declaration//GEN-END:variables
 }
